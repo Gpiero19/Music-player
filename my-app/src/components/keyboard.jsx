df97de1744fadba4728b267ba5461a01e9bbf49f
Gian Piero Canevari <canevarigian@gmail.com>
2025-08-23 19:24:51 +0200

    logic working

--- my-app/src/components/keyboard.jsx
+++ my-app/src/components/keyboard.jsx
@@ -1,30 +1,53 @@
-import React from "react";
-// import { useWithSound } from "./useWithSound";
+import React, {useEffect} from "react";
+import TestSound from "../sounds/bass-drum.mp3";
+import Singing from "../sounds/singing-guy.mp3";
+import Redoble from "../sounds/redoble.mp3";
+import Farts from "../sounds/farts-value.mp3";
+import Crash from "../sounds/crash-drum.mp3";
 
 export default function Keyboard() {
+    const keySound = [
+        { name:"A", key: "A", sound: Singing },
+        { name:"S", key: "S", sound: Redoble },
+        { name:"D", key: "D", sound: Crash },
+        { name:"F", key: "F", sound: TestSound },
+        { name:"H", key: "H", sound: TestSound },
+        { name:"J", key: "J", sound: TestSound },
+        { name:"K", key: "K", sound: TestSound },
+        { name:"L", key: "L", sound: Farts },
+    ];
 
-    const handleKeyClick = (key) => {
-        console.log("Key pressed:", key);
-        // const { playSound } = useWithSound();
-        // playSound(key);
+    const playSound = (sound) => {
+        const audio = new Audio(sound);
+        audio.currentTime = 0
+        audio.play();
     };
 
-    const keys = [
-        { label: "A", onClick: () => handleKeyClick("A") },
-        { label: "S", onClick: () => handleKeyClick("S") },
-        { label: "D", onClick: () => handleKeyClick("D") },
-        { label: "F", onClick: () => handleKeyClick("F") },
-        { label: "H", onClick: () => handleKeyClick("H") },
-        { label: "J", onClick: () => handleKeyClick("J") },
-        { label: "K", onClick: () => handleKeyClick("K") },
-        { label: "L", onClick: () => handleKeyClick("L") },
-    ];
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            const key = event.key.toUpperCase();
+            const sound = keySound.find((k) => k.key === key);
+            console.log(sound, key);
+            if (sound) {
+                playSound(sound.sound);
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyDown);
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
 
     return (
         <div className="keyboard">
-            {keys.map((key) => (
-                <button className='keys' key={key.label} onClick={key.onClick}>
-                    {key.label}
+            {keySound.map((key) => (
+                <button 
+                className='keys' 
+                key={key.name} 
+                onClick={() => playSound(key.sound)}
+                >
+                    {key.name}
                 </button>
             ))}
         </div>
